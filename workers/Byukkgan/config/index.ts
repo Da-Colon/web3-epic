372705c9309f8f116a4f5960d084138e552f0b5a
David Colon <dcolonantonio@gmail.com>
2021-10-19 23:46:39 -0400

    added new packages for other services

---
+++ workers/Byukkgan/config/index.ts
@@ -0,0 +1,32 @@
+import chalk from "chalk";
+import { Config } from "../types/app";
+require("dotenv").config();
+
+const config: Config = {
+  port: process.env.PORT || "12587",
+  isDev: process.env.NODE_ENV === 'development',
+  etherscanURL: "https://api.etherscan.io/api",
+  coinGeckoURL: "https://api.coingecko.com/api/v3",
+  infuraURL: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+  database: {
+    host: process.env.POSTGRES_HOST || "localhost",
+    user: process.env.POSTGRES_USER || "postgres",
+    password: process.env.POSTGRES_PASSWORD,
+    name: process.env.POSTGRES_DB || "",
+    port: process.env.POSTGRES_PORT || "5432",
+    dialect: "postgres",
+  },
+  web3: {
+    chainId: "1",
+    network: "mainnet",
+    providerKeys: {
+      infura: process.env.INFURA_API_KEY,
+      alchemy: process.env.ALCHEMY_API_KEY,
+      etherscan: process.env.ETHERSCAN_API_KEY,
+    },
+  },
+};
+
+console.info(chalk.bold.blue(`[${process.env.NODE_ENV}] environment`));
+
+export default config;
