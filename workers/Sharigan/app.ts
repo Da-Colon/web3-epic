20686bebb26a2d227c2020f554c71d3c222f2646
David Colon <dcolonantonio@gmail.com>
2021-11-20 10:59:50 -0500

    updates application setting to single class

--- workers/Sharigan/app.ts
+++ workers/Sharigan/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
+
 import DBConnection from "./database/connection";
-import * as Application from "./config/app.init";
+import ExpressApplication from "./config/app.init";
 import Web3Provider from "./web3/Web3Provider"
 // todo :initialize redis
 
@@ -8,18 +9,20 @@ import Web3Provider from "./web3/Web3Provider"
   const app = express();
   const dbConnection = new DBConnection(app)
   const web3Provider = new Web3Provider(app);
-  
+  const expressApplication = new ExpressApplication(app);
   // initialize database
   await dbConnection.init();
+
   // initialize web3 provider
   web3Provider.init()
 
+  // Express Aplication 
   // initialize cors
-  Application.cors(app);
+  expressApplication.cors();
   // initialize services
-  Application.logging(app);
-  Application.encoding(app);
-  // initialize blockchain indexer routes
+  expressApplication.logging();
+  expressApplication.encoding();
   // initialize server
-  Application.init(app);
+  
+  expressApplication.init();
 })()
--- workers/Sharigan/config/app.init.ts
+++ workers/Sharigan/config/app.init.ts
@@ -5,51 +5,55 @@ import config from ".";
 import chalk from "chalk";
 import corsOptions from "cors";
 
-export function cors(app: express.Application) {
-  app.use(corsOptions());
-}
-
-export function logging(app: express.Application) {
-  app.use(morgan("dev"));
-}
-
-export function encoding(app: express.Application) {
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-}
-
-export function normalizePort(val: string) {
-  const port = parseInt(val, 10);
-  if (isNaN(port)) return val;
-  if (port >= 0) return port;
-  return false;
-}
-
-function onError(error: any) {
-  if (error.syscall !== "listen") {
-    throw error;
+export default class ExpressApplication {
+  app: express.Application;
+  constructor(app: express.Application) {
+    this.app = app;
   }
-  const bind = typeof config.port === "string" ? "Pipe " + config.port : "Port " + config.port;
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-    default:
+
+  cors() {
+    this.app.use(corsOptions());
+  }
+  logging() {
+    this.app.use(morgan("dev"));
+  }
+  encoding() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  private onError(error: any) {
+    if (error.syscall !== "listen") {
       throw error;
+    }
+    const bind = typeof config.port === "string" ? "Pipe " + config.port : "Port " + config.port;
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case "EACCES":
+        console.error(bind + " requires elevated privileges");
+        process.exit(1);
+      case "EADDRINUSE":
+        console.error(bind + " is already in use");
+        process.exit(1);
+      default:
+        throw error;
+    }
   }
-}
+  private normalizePort(val: string) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) return val;
+    if (port >= 0) return port;
+    return false;
+  }
 
-export async function init(app: express.Application) {
-  const server = http.createServer(app);
-  const port = normalizePort(config.port);
-  app.set("port", port);
-  server.listen(port);
-  server.on("error", onError);
-  server.on("listening", () => {
-    console.info(chalk.bold.white(`[web3-indexer] is listening on ${config.port}.`));
-  });
+  async init() {
+    const server = http.createServer(this.app);
+    const port = this.normalizePort(config.port);
+    this.app.set("port", port);
+    server.listen(port);
+    server.on("error", this.onError);
+    server.on("listening", () => {
+      console.info(chalk.bold.white(`[web3-indexer] is listening on ${config.port}.`));
+    });
+  }
 }
